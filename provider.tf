25aeb485856f982af2470c422141af725a49d7d9
Vashchuk <oleksandr.vashchuk@capgemini.com>
2022-09-12 12:10:00 +0200

    'thirds'

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("SA_key.json")
+  credentials = file("/var/lib/jenkins/workspace/terraform-pipeline/SA_key.json")
 
   project = "my-test-362010"
   region  = "europe-central2"
